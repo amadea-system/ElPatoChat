8d6958286400ca7ad2f4ba6d7ef9e5bed44c6782
Amadea <53259552+amadea-system@users.noreply.github.com>
2025-07-06 16:29:58 -0400

    Fix font issue

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite';
+import { defineConfig, searchForWorkspaceRoot } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // server.fs.strict
+  server: {
+    fs: {
+      // strict: false, // Allow access to files outside the project root
+      allow: [
+        // search up for workspace root
+        searchForWorkspaceRoot(process.cwd()),
+        // NOT GOOD, but needed to access the @fontsource-poppins-npm package in the Yarn cache
+        'C:/Users/Ashley/AppData/Local/Yarn/Berry/cache/'
+      ],
+    },
+  },
 });
