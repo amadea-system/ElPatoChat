3c95d9a6985e40b83cfe79ca365ee452d8f6049d
Amadea <53259552+amadea-system@users.noreply.github.com>
2025-07-23 13:29:12 -0400

    Cleaned up useTwitchChat and made printing status events to chat configurable

--- front/src/hooks/useTwitchChat/useTwitchChat.tsx
+++ front/src/hooks/useTwitchChat/useTwitchChat.tsx
@@ -13,9 +13,90 @@ import { createNewMessage } from '../../utils/createNewMessage';
 
 const MAX_MESSAGES = 20;
 
+/* ----------- Types ---------- */
 type OnChatMessageEventHandler = (channel: string, user:string, text:string, msg: TwurpleChatMessage) => Promise<void>;
 
-export const useTwitchChat = (channel: UserInformation) => {
+type ChatStatusEventIDPrefix = 
+      'chat-auth-failure'            // chat.onAuthenticationFailure
+    | 'chat-auth-success'            // chat.onAuthenticationSuccess
+    | 'chat-connect-success'         // chat.onConnect
+    | 'chat-disconnect-success';     // chat.onDisconnect
+
+type EventSubStatusEventIDPrefix = 
+      'subscription-create-success'  // eventSubListener.onSubscriptionCreateSuccess  (Generic Event ID)
+    | 'subscription-create-failure'  // eventSubListener.onSubscriptionCreateFailure  (Generic Event ID)
+    | 'user-socket-connect'          // eventSubListener.onUserSocketConnect
+    | 'user-socket-disconnect';      // eventSubListener.onUserSocketDisconnect
+
+type StatusEventIDPrefix = ChatStatusEventIDPrefix | EventSubStatusEventIDPrefix;
+
+/* ----- Configs ----- */
+const MAX_MESSAGES = 20;
+
+// This is a list of status events that will be displayed in the chat.
+// If you want to prevent a status event from being displayed in the chat, remove it from this list.
+const STATUS_EVENTS_TO_DISPLAY_IN_CHAT: StatusEventIDPrefix[] = [
+  // Chat Status Events
+  'chat-auth-failure',
+  // 'chat-auth-success',
+  'chat-connect-success',
+  'chat-disconnect-success',
+
+
+
+/* ---------- Helper Functions ---------- */
+
+/**
+ * Helper function to remove any similar messages from the chat messages state.
+ * @param messageIds A message ID to remove. Strings or Regex patterns can be used.
+ *                   If string, it will remove messages that start with the string.
+ * @return void
+ */
+const removeMessagesById = (setChatMessages: React.Dispatch<React.SetStateAction<Array<ChatMessageData> | []>>, messageId: string | RegExp) => {
+  setChatMessages((msgs) => msgs.filter(msg => {
+    if (typeof messageId === 'string') {
+      return !msg.id.startsWith(messageId);
+    } else {
+      return !messageId.test(msg.id);
+    }
+  }));
+};
+
+
+const handleStatusEvent = (
+  setChatMessages: React.Dispatch<React.SetStateAction<Array<ChatMessageData> | []>>,
+  statusEventID: StatusEventIDPrefix,
+  removePastMessages: boolean = true,
+  content: string = '',
+  additionalID: string | null = null
+): void => {
+  if (!STATUS_EVENTS_TO_DISPLAY_IN_CHAT.includes(statusEventID)) {
+    return;
+  }
+
+  const msgIDPrefix = additionalID ? `${statusEventID}-${additionalID}` : statusEventID;
+  const uniqueId = `${msgIDPrefix}-${crypto.randomUUID()}`;
+  const finalContent = content || `Event: ${msgIDPrefix} @ ${new Date().toLocaleTimeString('en-US', { timeZone: 'UTC', hour12: false })}`;
+
+  const newMessage: ChatMessageData = createNewMessage({
+    id: uniqueId,
+    content: finalContent,
+    userDisplayName: 'Hibiki The Chat',  // TODO: Don't hardcode the System Message Username
+    systemMessage: true
+  });
+
+  if (removePastMessages) {
+    removeMessagesById(setChatMessages, `${msgIDPrefix}-`);
+  }
+  
+  setChatMessages((msgs) => (
+    [newMessage, ...msgs].slice(0, MAX_MESSAGES)
+  ));
+};
+
+/* ---------- Main Hook ---------- */
+
+export const useTwitchChat = (channelInfo: UserInformation) => {
   const configuration = useConfiguration(state => state);
 
   const { 
@@ -23,8 +104,8 @@ export const useTwitchChat = (channel: UserInformation) => {
     onRemoveMessage: ttsRemoveMessage,
     speak: ttsSpeak
   } = useTTS();
-  const customEmotes = useCustomEmotes(channel.id);
-  const { parseBadges } = useBadges(channel.id);
+  const customEmotes = useCustomEmotes(channelInfo.id);
+  const { parseBadges } = useBadges(channelInfo.id);
   const { getPronounsFromTwitchName } = usePronouns();
   const [chat, setChat] = useState<ChatClient | null>(null);
   const [chatMessages, setChatMessages] = useState<Array<ChatMessageData> | []>([]);
@@ -32,19 +113,23 @@ export const useTwitchChat = (channel: UserInformation) => {
   const onChatClearRef = useRef<(() => void) | null>(null);
   const onMessageRemovedRef = useRef<((messageId: string) => void) | null>(null);
 
+  /* ----- Chat Client Initialization ----- */
   useEffect(() => {
     const chatClient = new ChatClient({
-      channels: [channel.login]
+      channels: [channelInfo.login]
     });
     chatClient.connect();
     setChat(chatClient);
+    console.log(`Twitch Chat Client Connected to channel: ${channelInfo.login}`);
+    console.log('Twitch Channel Details:', channelInfo);
 
     return () => {
       chatClient.quit();
       setChat(null);
     };
-  }, [channel]);
+  }, [channelInfo]);
 
+  /* ----- Chat Event Handlers ----- */
   // avoids reconnection on callback changes by keeping them on ref
   // twurple does not allow us to disconnect events for some reason... :/ so this works
   useEffect(() => {
@@ -100,62 +185,30 @@ export const useTwitchChat = (channel: UserInformation) => {
 
     chat.onAuthenticationFailure((text, retryCount) => {
       console.error(`Authentication failed: ${text}. Retry count: ${retryCount}`);
-      const uniqueId = `auth-failure-${crypto.randomUUID()}`;
-      const newMessage: ChatMessageData = createNewMessage({
-        id: uniqueId,
-        // content: 'Twitch Chat Authentication Failure...',
-        content: `Twitch Chat Authentication Failure @ ${new Date().toLocaleTimeString('en-US', { timeZone: 'UTC', hour12: false })}`,
-        userDisplayName: 'Hibiki The Chat',  // TODO: Don't hardcode the System Message Username
-        systemMessage: true
-      });
-      setChatMessages((msgs) => (
-        [newMessage, ...msgs].slice(0, MAX_MESSAGES)
-      ));
+      const eventMsgIDPrefix = 'chat-auth-failure';
+      const chatMsg = `Twitch Chat Authentication Failure @ ${new Date().toLocaleTimeString('en-US', { timeZone: 'UTC', hour12: false })}`;
+      handleStatusEvent(setChatMessages, eventMsgIDPrefix, true, chatMsg);
     });
 
     chat.onAuthenticationSuccess(() => {
       console.log('Twitch Chat Authentication Success');
-      const uniqueId = `auth-success-${crypto.randomUUID()}`;
-      const newMessage: ChatMessageData = createNewMessage({
-        id: uniqueId,
-        // content: 'Twitch Chat Authentication Success...',
-        content: `Twitch Chat Authentication Success @ ${new Date().toLocaleTimeString('en-US', { timeZone: 'UTC', hour12: false })}`,
-        userDisplayName: 'Hibiki The Chat',  // TODO: Don't hardcode the System Message Username
-        systemMessage: true
-      });
-      setChatMessages((msgs) => (
-        [newMessage, ...msgs].slice(0, MAX_MESSAGES)
-      ));
+      const eventMsgIDPrefix = 'chat-auth-success';
+      const chatMsg = `Twitch Chat Authentication Success @ ${new Date().toLocaleTimeString('en-US', { timeZone: 'UTC', hour12: false })}`;
+      handleStatusEvent(setChatMessages, eventMsgIDPrefix, true, chatMsg);
     });
 
     chat.onConnect(() => {
       console.log('Twitch Chat Connected');
-      const uniqueId = `connect-success-${crypto.randomUUID()}`;
-      const newMessage: ChatMessageData = createNewMessage({
-        id: uniqueId,
-        // content: 'Twitch Chat Connected...',
-        content: `Twitch Chat Connected @ ${new Date().toLocaleTimeString('en-US', { timeZone: 'UTC', hour12: false })}`,
-        userDisplayName: 'Hibiki The Chat',  // TODO: Don't hardcode the System Message Username
-        systemMessage: true
-      });
-      setChatMessages((msgs) => (
-        [newMessage, ...msgs].slice(0, MAX_MESSAGES)
-      ));
+      const eventMsgIDPrefix = 'chat-connect-success';
+      const chatMsg = `Twitch Chat Connected @ ${new Date().toLocaleTimeString('en-US', { timeZone: 'UTC', hour12: false })}`;
+      handleStatusEvent(setChatMessages, eventMsgIDPrefix, true, chatMsg);
     });
 
     chat.onDisconnect((manually, reason) => {
       console.log(`Twitch Chat Disconnected. Manually: ${manually}, Reason: ${reason}`);
-      const uniqueId = `disconnect-success-${crypto.randomUUID()}`;
-      const newMessage: ChatMessageData = createNewMessage({
-        id: uniqueId,
-        // content: 'Twitch Chat Disconnected...',
-        content: `Twitch Chat Disconnected @ ${new Date().toLocaleTimeString('en-US', { timeZone: 'UTC', hour12: false })}`,
-        userDisplayName: 'Hibiki The Chat',  // TODO: Don't hardcode the System Message Username
-        systemMessage: true
-      });
-      setChatMessages((msgs) => (
-        [newMessage, ...msgs].slice(0, MAX_MESSAGES)
-      ));
+      const eventMsgIDPrefix = 'chat-disconnect-success';
+      const chatMsg = `Twitch Chat Disconnected @ ${new Date().toLocaleTimeString('en-US', { timeZone: 'UTC', hour12: false })}`;
+      handleStatusEvent(setChatMessages, eventMsgIDPrefix, true, chatMsg);
     });
 
   }, [chat]);
