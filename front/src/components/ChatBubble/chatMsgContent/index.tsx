40da416c33027caebfe8cda7dabc0aef97c3c6ca
Amadea <53259552+amadea-system@users.noreply.github.com>
2025-07-27 14:26:31 -0400

    Make emotes scale to med size when there is 2-4 emotes in the message.

--- front/src/components/ChatBubble/chatMsgContent/index.tsx
+++ front/src/components/ChatBubble/chatMsgContent/index.tsx
@@ -2,6 +2,16 @@ import { MessagePart } from '../../../types';
 import Emote from '../emote';
 import * as S from './styles';
 
+/* ----- Configs ----- */
+
+// TODO: Make this themeable or configurable via URLParams
+// Set the max number of emotes that can be displayed at a given scale
+const EmoteScaleLimits = {
+  Large: 1,
+  Medium: 4
+  // Small is default, no limit
+};
+
 export type ChatMsgContentProps = {
   messageParts: Array<MessagePart>,
   userColor: string | undefined
@@ -10,6 +20,8 @@ export type ChatMsgContentProps = {
 const ChatMsgContent = ({ messageParts, userColor = 'black' }: ChatMsgContentProps) => {
   if (messageParts.length === 0) return null;
 
+  // TODO: `parent` is not a good name, as it doen't actually represent a parent of the message, but rather a header or a special part of the message
+  //       that shouldn't count towards the emote scaling and is not part of the main message content div.
   const parentTypes = ['reply', 'redeption', 'follow'];
   const parentPart = messageParts.find(part => parentTypes.includes(part.type));
   const firstPart = parentPart ?? messageParts[0];
@@ -17,18 +29,26 @@ const ChatMsgContent = ({ messageParts, userColor = 'black' }: ChatMsgContentPro
 
   const renderPart = ({ content, type, customEmote }: MessagePart, index: number) => {
     switch (type) {
-    case 'emote':
+    case 'emote': {
+      const scale = (() => {
+        const partCountModifier = parentTypes.includes(firstPart.type) ? 1 : 0;  // If the message has a part that is a reply, redemption, or follow, we increase the required number by one to account for the parent part.
+        if (messageParts.length <= (EmoteScaleLimits.Large + partCountModifier)) {
+          return 3;
+        } else if (messageParts.length <= (EmoteScaleLimits.Medium + partCountModifier)) {
+          return 2;
+        }
+        return 1;
+      })();
       return <Emote
         key={index}
         id={content}
         customEmote={customEmote} 
-        // scale={messageParts.length === 1 ? 3 : 1}
-        // scale={((firstPart.type === 'reply' && messageParts.length === 2) || messageParts.length === 1) ? 3 : 1}
-        scale={((parentTypes.includes(firstPart.type) && messageParts.length === 2) || messageParts.length === 1) ? 3 : 1}
+        scale={scale}
         alignCorrection={
           messageParts.filter(p => p.type === 'reply').length > 1
         }
       />;
+    }
     case 'redeption':
       return <S.Redemption $userColor={userColor} key={index}>{ content }</S.Redemption>;
     case 'reply':
